1e660a2077e92dd161f3a824ba4cd58de7d7dc07
Rohan Awhad <rohanavad007@gmail.com>
2024-09-22 12:35:43 -0700

    now able to draw boundaries around pupil and eyelibds and brows; figuring out how to get those values and make sense of it

--- main.py
+++ main.py
@@ -1,6 +1,98 @@
 import cv2
 import mediapipe as mp
 
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+face_landmarks_list is a list of all the faces, which in turn have the landmarks
+
+class Blendshapes(enum.IntEnum):
+  """The 52 blendshape coefficients."""
+
+  NEUTRAL = 0
+  BROW_DOWN_LEFT = 1
+  BROW_DOWN_RIGHT = 2
+  BROW_INNER_UP = 3
+  BROW_OUTER_UP_LEFT = 4
+  BROW_OUTER_UP_RIGHT = 5
+  CHEEK_PUFF = 6
+  CHEEK_SQUINT_LEFT = 7
+  CHEEK_SQUINT_RIGHT = 8
+  EYE_BLINK_LEFT = 9
+  EYE_BLINK_RIGHT = 10
+  EYE_LOOK_DOWN_LEFT = 11
+  EYE_LOOK_DOWN_RIGHT = 12
+  EYE_LOOK_IN_LEFT = 13
+  EYE_LOOK_IN_RIGHT = 14
+  EYE_LOOK_OUT_LEFT = 15
+  EYE_LOOK_OUT_RIGHT = 16
+  EYE_LOOK_UP_LEFT = 17
+  EYE_LOOK_UP_RIGHT = 18
+  EYE_SQUINT_LEFT = 19
+  EYE_SQUINT_RIGHT = 20
+  EYE_WIDE_LEFT = 21
+  EYE_WIDE_RIGHT = 22
+
+'''
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  face_landmarks_list = detection_result.face_landmarks
+
+
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected faces to visualize.
+  for idx in range(len(face_landmarks_list)):
+    face_landmarks = face_landmarks_list[idx]
+
+    # Draw the face landmarks.
+    face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    face_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
+    ])
+
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp.solutions.drawing_styles
+        .get_default_face_mesh_tesselation_style())
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp.solutions.drawing_styles
+        .get_default_face_mesh_contours_style())
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=mp.solutions.drawing_styles
+          .get_default_face_mesh_iris_connections_style())
+
+  return annotated_image
+
+# STEP 1: Import the necessary modules.
+# TODO: move the imports above
+
+# STEP 2: Create an FaceLandmarker object.
+base_options = python.BaseOptions(model_asset_path='face_landmarker.task')
+options = vision.FaceLandmarkerOptions(base_options=base_options,
+                                       output_face_blendshapes=True,
+                                       output_facial_transformation_matrixes=True,
+                                       num_faces=1)
+detector = vision.FaceLandmarker.create_from_options(options)
+
 # Initialize MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
@@ -13,45 +105,31 @@ face_mesh = mp_face_mesh.FaceMesh(
 # Initialize drawing utility
 mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+iris_drawing_spec = mp_drawing.DrawingSpec(thickness=2, circle_radius=2)
 
 # Capture video from webcam
 cap = cv2.VideoCapture(2)
 
 while cap.isOpened():
-    success, image = cap.read()
+    success, cv2_image = cap.read()
     if not success:
         print("Ignoring empty camera frame.")
         continue
 
-    # Convert the image to RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(cv2_image))
+    detection_result = detector.detect(image)
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
     
-    # To improve performance, optionally mark the image as not writeable
-    image.flags.writeable = False
-    
-    # Process the image and find faces
-    results = face_mesh.process(image)
-    
-    # Draw the face mesh annotations on the image
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_face_landmarks:
-        for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                image=image,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec=drawing_spec,
-                connection_drawing_spec=drawing_spec)
-
     # Display the image
-    cv2.imshow('My Webcam', image)
+    annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+    height = 200
+    width = int(annotated_image.shape[1] * height / annotated_image.shape[0])
+    resized_image = cv2.resize(annotated_image, (width, height))
+
+    cv2.imshow('My Webcam', resized_image)
     if cv2.waitKey(5) & 0xFF == 27:  # Press 'ESC' to exit
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-'''
-'''
